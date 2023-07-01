e2a63f3edba48f986ad1576b3358b17b3e5156a7
a-r-t <alexthimineur@gmail.com>
2023-07-01 15:50:47 -0400

    icons

--- src/Frame.java
+++ src/Frame.java
@@ -1,5 +1,10 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
@@ -14,6 +19,7 @@ public class Frame {
     public Frame() {
         setSwingStyle();
         frame = new JFrame();
+        frame.setIconImages(getIcons());
         mainPanel = new JPanel();
 
         selectionsHolder = new SelectionsHolder();
@@ -40,5 +46,33 @@ public class Frame {
         } catch (ClassNotFoundException | UnsupportedLookAndFeelException | IllegalAccessException | InstantiationException e) {
             e.printStackTrace();
         }
+    }
+
+    private ArrayList<BufferedImage> getIcons() {
+        ArrayList<BufferedImage> icons = new ArrayList<>();
+        try {
+            // credit to Freepik for the icon files: https://www.flaticon.com/authors/freepik
+            // icon file found here: https://www.flaticon.com/free-icon/paint-palette_2970785?term=paint&page=1&position=5&origin=tag&related_id=2970785#
+            BufferedImage icon16 = ImageIO.read(Frame.class.getResource("/icon-16.png"));
+            BufferedImage icon24 = ImageIO.read(Frame.class.getResource("/icon-24.png"));
+            BufferedImage icon32 = ImageIO.read(Frame.class.getResource("/icon-32.png"));
+            BufferedImage icon64 = ImageIO.read(Frame.class.getResource("/icon-64.png"));
+            BufferedImage icon128 = ImageIO.read(Frame.class.getResource("/icon-128.png"));
+            BufferedImage icon256 = ImageIO.read(Frame.class.getResource("/icon-256.png"));
+            BufferedImage icon512 = ImageIO.read(Frame.class.getResource("/icon-512.png"));
+
+            icons.add(icon16);
+            icons.add(icon24);
+            icons.add(icon32);
+            icons.add(icon64);
+            icons.add(icon128);
+            icons.add(icon256);
+            icons.add(icon512);
+
+        }
+        catch (IOException e) {
+            System.out.println("Unable to load icon images! Using defaults.");
+        }
+        return icons;
     }
 }
